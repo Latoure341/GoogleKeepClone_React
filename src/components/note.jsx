424364cacd1d23a772fa67b90973ad8abd07dc6b
latoure <tukamothupi@gmail.com>
2026-04-04 13:50:35 +0200

    adding responsive layout

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -27,68 +27,63 @@ const Note = (props) => {
   }
 
   return (
-    <>
-      <div className="notes">
-        <div
-          className="note"
-          id={note.id}
-          onMouseOver={handlerMouserOver}
-          onMouseOut={handleMouseOut}
-          onClick={handleNoteClick}
+    <div
+      className="note"
+      id={note.id}
+      onMouseOver={handlerMouserOver}
+      onMouseOut={handleMouseOut}
+      onClick={handleNoteClick}
+    >
+      {isHover && (
+        <span
+          className="material-symbols-outlined check-circle"
+          id="check-circle"
         >
-          {isHover && (
-            <span
-              className="material-symbols-outlined check-circle"
-              id="check-circle"
-            >
-              {" "}
-              check_circle
-            </span>
-          )}
-          <div className="title">{note.title}</div>
-          <div className="text">{note.text}</div>
-          <div className="note-footer" style={{visibility: isHover ? "visible" : "hidden"}}>
-            <div className="tooltip">
-              <span className="material-symbols-outlined hover small-icon">
-                add_alert
-              </span>
-              <span className="tooltip-text">Remind me</span>
-            </div>
-            <div className="tooltip">
-              <span className="material-symbols-outlined hover small-icon">
-                person_add
-              </span>
-              <span className="tooltip-text">Collaborator</span>
-            </div>
-            <div className="tooltip">
-              <span className="material-symbols-outlined hover small-icon">
-                palette
-              </span>
-              <span className="tooltip-text">Change Color</span>
-            </div>
-            <div className="tooltip">
-              <span className="material-symbols-outlined hover small-icon">
-                image
-              </span>
-              <span className="tooltip-text">Add Image</span>
-            </div>
-            <div className="tooltip">
-              <span className="material-symbols-outlined hover small-icon archive" onClick={handleArchive}>
-                archive
-              </span>
-              <span className="tooltip-text archive" >Archive</span>
-            </div>
-            <div className="tooltip">
-              <span className="material-symbols-outlined hover small-icon">
-                more_vert
-              </span>
-              <span className="tooltip-text">More</span>
-            </div>
-          </div>
+          {" "}
+          check_circle
+        </span>
+      )}
+      <div className="title">{note.title}</div>
+      <div className="text">{note.text}</div>
+      <div className="note-footer" style={{visibility: isHover ? "visible" : "hidden"}}>
+        <div className="tooltip">
+          <span className="material-symbols-outlined hover small-icon">
+            add_alert
+          </span>
+          <span className="tooltip-text">Remind me</span>
         </div>
-        <span className="tooltip-text">Add Image</span>
+        <div className="tooltip">
+          <span className="material-symbols-outlined hover small-icon">
+            person_add
+          </span>
+          <span className="tooltip-text">Collaborator</span>
+        </div>
+        <div className="tooltip">
+          <span className="material-symbols-outlined hover small-icon">
+            palette
+          </span>
+          <span className="tooltip-text">Change Color</span>
+        </div>
+        <div className="tooltip">
+          <span className="material-symbols-outlined hover small-icon">
+            image
+          </span>
+          <span className="tooltip-text">Add Image</span>
+        </div>
+        <div className="tooltip">
+          <span className="material-symbols-outlined hover small-icon archive" onClick={handleArchive}>
+            archive
+          </span>
+          <span className="tooltip-text archive" >Archive</span>
+        </div>
+        <div className="tooltip">
+          <span className="material-symbols-outlined hover small-icon">
+            more_vert
+          </span>
+          <span className="tooltip-text">More</span>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/notes.jsx
+++ src/components/notes.jsx
@@ -1,18 +1,39 @@
+import Masonry from "react-masonry-css";
 import "./component_styles/notes.css";
-import Note from "./note"
+import Note from "./note";
 import EmptyNotesList from "./EmptyNotesList";
 
+const breakpointColumnsObj = {
+  default: 4,
+  1100: 3,
+  800: 2,
+  500: 1,
+};
+
 function Notes(props) {
-  const { notes, deleteNote, toggleModal, setSelectedNote } = props
+  const { notes, deleteNote, toggleModal, setSelectedNote } = props;
 
   return (
-    <>
     <div className="notes">
-        {notes.length === 0 && <EmptyNotesList />}
-        {notes.length !== 0 && notes.map((note, index) => <Note key={index} note={note} deleteNote={deleteNote} toggleModal={toggleModal} setSelectedNote={setSelectedNote}/>)}
-      </div>
-        
-    </>
+      {notes.length === 0 && <EmptyNotesList />}
+      {notes.length !== 0 && (
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="notes-masonry"
+          columnClassName="notes-masonry_column"
+        >
+          {notes.map((note) => (
+            <Note
+              key={note.id}
+              note={note}
+              deleteNote={deleteNote}
+              toggleModal={toggleModal}
+              setSelectedNote={setSelectedNote}
+            />
+          ))}
+        </Masonry>
+      )}
+    </div>
   );
 }
 
